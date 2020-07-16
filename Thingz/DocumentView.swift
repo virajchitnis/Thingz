bfbd0f5573f4fcd62adf744df9b77ff480f103ce
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-16 19:36:36 +0100

    Initial Commit

---
+++ Thingz/DocumentView.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentView.swift
+//  Thingz
+//
+//  Created by Viraj Chitnis on 7/16/20.
+//  Copyright © 2020 Viraj Chitnis. All rights reserved.
+//
+
+import SwiftUI
+
+struct DocumentView: View {
+    var document: UIDocument
+    var dismiss: () -> Void
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("File Name")
+                    .foregroundColor(.secondary)
+
+                Text(document.fileURL.lastPathComponent)
+            }
+
+            Button("Done", action: dismiss)
+        }
+    }
+}
