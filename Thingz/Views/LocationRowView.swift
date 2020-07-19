b22c8dda313288d75d698f7fede1f28978960ac1
Viraj Chitnis <chitnisviraj@gmail.com>
2020-07-20 00:24:45 +0100

    Display photos in the LocationListView

--- Thingz/Views/LocationRowView.swift
+++ Thingz/Views/LocationRowView.swift
@@ -12,21 +12,37 @@ struct LocationRowView: View {
     var location: Location
     
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text("\(self.location.name)")
-                    .font(.headline)
-                Text("\(self.location.barcode)")
-                    .font(.caption)
-                    .foregroundColor(Color.gray)
-                Text("\(self.location.description)")
-                    .font(.subheadline)
-                    .foregroundColor(Color.gray)
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("\(self.location.name)")
+                        .font(.headline)
+                    Text("\(self.location.barcode)")
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                    Text("\(self.location.description)")
+                        .font(.subheadline)
+                        .foregroundColor(Color.gray)
+                }
+                Spacer()
+                Text("\(self.location.things.count) things")
+                    .font(.body)
+                    .multilineTextAlignment(.trailing)
             }
-            Spacer()
-            Text("\(self.location.things.count) things")
-                .font(.body)
-                .multilineTextAlignment(.trailing)
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(self.location.photos, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .stroke(Color.gray, lineWidth: 2)
+                            )
+                    }
+                }.padding()
+            }
         }
     }
 }
